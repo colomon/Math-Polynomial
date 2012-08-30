44349fddda7ef41fed7d41515e5037159ebd6245
Solomon Foster <colomon@gmail.com>
2012-08-29 20:52:11 -0400

    Make it all look more like my current p6 code.  :)

--- lib/Math/Polynomial.pm
+++ lib/Math/Polynomial.pm
@@ -1,45 +1,35 @@
 use v6;
 
-class Math::Polynomial
-{
+class Math::Polynomial {
     has @.coefficients;
 
-    multi method new (*@x)
-    {
-        self.new: @x.item;
+    multi method new (*@coefficients) {
+        self.new(@coefficients);
     }
 
-    multi method new (@x is copy)
-    {
-        while @x.elems > 1 && @x[*-1].abs < 1e-13
-        {
-            # say @x.perl;
-            # say @x[*-1];
+    multi method new (@x is copy) {
+        while @x > 1 && @x[*-1].abs < 1e-13 {
             @x.pop;
         }
 
-        @x.push: 0 if @x.elems == 0;
+        @x.push(0) if @x.elems == 0;
 
-        self.bless: *, coefficients => @x;
+        self.bless(*, coefficients => @x);
     }
 
-    method Str() returns Str
-    {
-        @.coefficients.kv.map({ "$^value x^$^key" }).reverse.join: ' + ';
+    method Str() returns Str {
+        @.coefficients.kv.map({ "$^value x^$^key" }).reverse.join(' + ');
     }
 
-    method perl() returns Str
-    {
-        "Math::Polynomial" ~ ".new(" ~ @.coefficients».perl.join(', ') ~ ")";
+    method perl() returns Str {
+        "Math::Polynomial.new(" ~ @.coefficients».perl.join(', ') ~ ")";
     }
 
-    method evaluate($x)
-    {
-        @.coefficients.reverse.reduce: { $^a * $x + $^b };
+    method evaluate($x) {
+        @.coefficients.reverse.reduce({ $^a * $x + $^b });
     }
 
-    multi sub infix:<+>(Math::Polynomial $a, Math::Polynomial $b) is export(:DEFAULT)
-    {
+    multi sub infix:<+>(Math::Polynomial $a, Math::Polynomial $b) is export(:DEFAULT) {
         my $max =   $a.coefficients.elems
                 max $b.coefficients.elems;
 
@@ -47,42 +37,35 @@ class Math::Polynomial
                   Z+ $b.coefficients, 0 xx * )[^$max];
     }
 
-    multi sub infix:<+>(Math::Polynomial $a, $b) is export(:DEFAULT)
-    {
+    multi sub infix:<+>(Math::Polynomial $a, $b) is export(:DEFAULT) {
         my @ac = $a.coefficients;
         @ac[0] += $b;
-        return $a.new: @ac;
+        return $a.new(@ac);
     }
 
-    multi sub infix:<+>($b, Math::Polynomial $a) is export(:DEFAULT)
-    {
+    multi sub infix:<+>($b, Math::Polynomial $a) is export(:DEFAULT) {
         $a + $b;
     }
 
-    multi sub prefix:<->(Math::Polynomial $a) is export(:DEFAULT)
-    {
-        $a.new: $a.coefficients.map({-$_});
+    multi sub prefix:<->(Math::Polynomial $a) is export(:DEFAULT) {
+        $a.new($a.coefficients.map({-$_}));
     }
 
-    multi sub infix:<->(Math::Polynomial $a, Math::Polynomial $b) is export(:DEFAULT)
-    {
+    multi sub infix:<->(Math::Polynomial $a, Math::Polynomial $b) is export(:DEFAULT) {
         -$b + $a;
     }
 
-    multi sub infix:<->(Math::Polynomial $a, $b) is export(:DEFAULT)
-    {
+    multi sub infix:<->(Math::Polynomial $a, $b) is export(:DEFAULT) {
         my @ac = $a.coefficients;
         @ac[0] -= $b;
-        return $a.new: @ac;
+        return $a.new(@ac);
     }
 
-    multi sub infix:<->($b, Math::Polynomial $a) is export(:DEFAULT)
-    {
+    multi sub infix:<->($b, Math::Polynomial $a) is export(:DEFAULT) {
         -$a + $b;
     }
 
-    multi sub infix:<*>(Math::Polynomial $a, Math::Polynomial $b) is export(:DEFAULT)
-    {
+    multi sub infix:<*>(Math::Polynomial $a, Math::Polynomial $b) is export(:DEFAULT) {
         my @coef;
         for     $a.coefficients.kv -> $ak, $av {
             for $b.coefficients.kv -> $bk, $bv {
@@ -90,21 +73,18 @@ class Math::Polynomial
             }
         }
 
-        return $a.new: @coef;
+        return $a.new(@coef);
     }
 
-    multi sub infix:<*>(Math::Polynomial $a, $b) is export(:DEFAULT)
-    {
-        $a.new: $a.coefficients >>*>> $b;
+    multi sub infix:<*>(Math::Polynomial $a, $b) is export(:DEFAULT) {
+        $a.new($a.coefficients »*» $b);
     }
 
-    multi sub infix:<*>($b, Math::Polynomial $a) is export(:DEFAULT)
-    {
-        $a.new: $a.coefficients >>*>> $b;
+    multi sub infix:<*>($b, Math::Polynomial $a) is export(:DEFAULT) {
+        $a.new($a.coefficients »*» $b);
     }
 
-    multi sub infix:</>(Math::Polynomial $a, $b) is export(:DEFAULT)
-    {
-        $a.new: $a.coefficients >>/>> $b;
+    multi sub infix:</>(Math::Polynomial $a, $b) is export(:DEFAULT) {
+        $a.new($a.coefficients »/» $b);
     }
 }
